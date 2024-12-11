0f67a0ba3356efee22a5b569c636fc9ab363595c
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2024-12-11 07:36:06 +0200

    new way from whatsapp

--- hours_to_seconds.py
+++ hours_to_seconds.py
@@ -1,5 +1,3 @@
 hours = 2
 seconds=3600*hours
-y = "seconds = hours * 3600"
-z = "seconds=3600*hours"
-print (hours, " hour(s) is ", seconds, " seconds.")
+print (f"{hours} hour(s) is {seconds} seconds.")
