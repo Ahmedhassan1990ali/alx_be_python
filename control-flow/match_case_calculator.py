c70ee9cef8809458d0501e08c2bad78a6516214a
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2024-12-16 16:07:14 +0200

    m

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,4 +6,5 @@ match operation:
     case "-": result = num1 - num2 ;print (f"The result is {result}.")         
     case "*": result = num1 * num2 ;print (f"The result is {result}.")         
     case "/" if num2 == 0 :  print("Cannot divide by zero." )
-    case "/": result = num1 / num2 ;print (f"The result is {result}.")         
+    case "/": result = num1 / num2 ;print (f"The result is {result}.")      
+    case _  : print ("Error")   
