2dda7b070900ab8d608df531149cc380485ddc60
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-01-20 00:11:07 +0200

    9

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,7 @@ class Test_simple_calculato(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
     
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc .add(2,3),5)
 
     def test_subtract(self):
