b2d9550b411a9cd96d50a0c9ae2b0eb0def40cfc
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-01-19 23:30:03 +0200

    6

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,32 @@
+class Book:
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.__is_checked_out__ = False
+
+    
+
+class Library:
+    __books__ = []
+
+    def add_book(self, book):
+        if (type(book)= Book):
+            self.__books__.append(book)
+    
+    def check_out_book(self, title):
+        for i in self.__books__:
+            if title == i.title and i.__is_checked_out__ == False:
+                i.__is_checked_out__ = True
+
+    def return_book(self, title):
+        for i in self.__books__:
+            if title == i.title and i.__is_checked_out__ == True:
+                i.__is_checked_out__ = False
+
+    def list_available_books(self):
+        available = []
+        for i in self.__books__:
+            if i.__is_checked_out__ == False:
+                available.append(i.title)
+        return available
